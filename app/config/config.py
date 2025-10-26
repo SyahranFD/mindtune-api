e27046acdd3379168fb3c06f3d7501932258c759
SyahranFD <fadhilrafa1@gmail.com>
2025-10-27 01:34:21 +0700

    feat: [SF] change some code in config

--- app/config/config.py
+++ app/config/config.py
@@ -4,8 +4,8 @@ from urllib.parse import quote_plus
 DB_USER = "postgres"
 DB_PASS = quote_plus(os.getenv("DB_PASS", "admin"))
 DB_NAME = os.getenv("DB_NAME", "fastapi_mindtune_api")
-DB_HOST = "localhost"
-DB_PORT = "5433"
+DB_HOST = os.getenv("DB_HOST", "db")
+DB_PORT = os.getenv("DB_PORT", "5432")
 
 DB_POOLSIZE = 50
 DB_MAXOVERFLOW = 25
